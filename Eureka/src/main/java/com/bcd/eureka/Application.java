41b90a53b3ce0d7a82b33f4ec487191fb983224c
baichangda <471267877@qq.com>
2018-02-23 09:22:52 +0800

    c

---
+++ Eureka/src/main/java/com/bcd/eureka/Application.java
@@ -0,0 +1,14 @@
+package com.bcd.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
